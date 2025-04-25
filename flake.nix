b9bfabc8fdaf9dda5a43de38911e0cb046dd55c0
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2025-04-26 02:04:32 +0300

    Add Fish Shell

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    oh-my-bash = {
+      url = "github:ohmybash/oh-my-bash";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = { self, nixpkgs, flake-utils, ... }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -19,9 +23,14 @@
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            fish
+            pango
             gtkVersion
             meson
+            harfbuzz
+            cairo
             ninja
+            libadwaita
             pkg-config
             gobject-introspection
             glib
@@ -29,7 +38,14 @@
 
           shellHook = ''
             echo "GTK development environment ready."
-            export PKG_CONFIG_PATH="${gtkVersion.dev}/lib/pkgconfig"
+            echo "✨ Welcome to your GTK dev shell"
+            export PKG_CONFIG_PATH="${pkgs.gtk4.dev}/lib/pkgconfig:${pkgs.pango.dev}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig:${pkgs.harfbuzz.dev}/lib/pkgconfig:${pkgs.cairo.dev}/lib/pkgconfig:${pkgs.gdk-pixbuf.dev}/lib/pkgconfig:${pkgs.vulkan-loader.dev}/lib/pkgconfig:${pkgs.graphene.dev}/lib/pkgconfig"
+            if ! type fisher &>/dev/null; then
+              echo "Installing fisher plugin manager..."
+              curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+            fi
+            fisher install IlanCosman/tide@v6
+            fish
           '';
         };
       });
